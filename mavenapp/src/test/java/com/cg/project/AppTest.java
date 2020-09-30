7bc4d107fecd24d1b53d74406a129c6a2234ffaa
simplylearn git <chitra.shrivastava@gmail.com>
2020-09-30 12:09:22 +0530

    movement of project to github

--- mavenapp/src/test/java/com/cg/project/AppTest.java
+++ mavenapp/src/test/java/com/cg/project/AppTest.java
@@ -1,38 +1,25 @@
 package com.cg.project;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	@Test
+	public void test1() {
+	System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+	System.out.println("test2");
+	}
+	@Test
+	public void test3() {
+	System.out.println("test3");
+	}
+	@Test
+	public void test4() {
+	System.out.println("test4");
+	}
 }
