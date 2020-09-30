c3ef80e3b97ded610755822f1809e23ad014af6d
simplylearn git <chitra.shrivastava@gmail.com>
2020-09-30 16:05:42 +0530

    main class movement

---
+++ mavenapp/src/main/java/com/cg/project/main/AppMain.java
@@ -0,0 +1,13 @@
+package com.cg.project.main;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+int value=10;
+value+=20;
+System.out.println(value);
+	}
+
+}
